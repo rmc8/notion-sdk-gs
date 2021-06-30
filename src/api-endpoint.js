565583c90e5ad5b69ad5c7e3a00a7f534b95029c
rmc8 <kmyashi@rmc-8.com>
2021-06-30 09:30:11 +0900

    [FIX] eslint max-len

--- src/api-endpoint.js
+++ src/api-endpoint.js
@@ -1,74 +1,82 @@
-class UsersEndpoint{
-    constructor(parent) {
-      this.parent = parent
-      this.path = "users"
-    }
-    list() {
-      return this.parent.request(this.path, "get", null)
-    }
-    retrieve(userId) {
-      return this.parent.request(`${this.path}/${userId}`, "get", null)
-    }
+class UsersEndpoint {
+  constructor(parent) {
+    this.parent = parent;
+    this.path = "users";
+  }
+  list() {
+    return this.parent.request(this.path, "get", null);
+  }
+  retrieve(userId) {
+    return this.parent.request(`${this.path}/${userId}`, "get", null);
+  }
 }
 
 class SearchEndpoint {
-    constructor(parent) {
-        function call(params=null){
-          return parent.request("search", "post", params)
-        }
-        return call
+  constructor(parent) {
+    function call(params = null) {
+      return parent.request("search", "post", params);
     }
+    return call;
+  }
 }
 
 class PagesEndpoint {
-    constructor(parent) {
-        this.parent = parent;
-        this.path = "pages"
-    }
-    create(params){
-      return this.parent.request(this.path, "post", params)
-    }
-    retrieve(pageId){
-      return this.parent.request(`${this.path}/${pageId}`, "get")
-    }
-    update(pageId, params){
-      return this.parent.request(`${this.path}/${pageId}`, "patch", params)
-    }
+  constructor(parent) {
+    this.parent = parent;
+    this.path = "pages";
+  }
+  create(params) {
+    return this.parent.request(this.path, "post", params);
+  }
+  retrieve(pageId) {
+    return this.parent.request(`${this.path}/${pageId}`, "get");
+  }
+  update(pageId, params) {
+    return this.parent.request(`${this.path}/${pageId}`, "patch", params);
+  }
 }
 
 class DatabasesEndpoint {
-    constructor(parent) {
-        this.parent = parent;
-        this.path = "databases"
-    }
-    list(){
-      return this.parent.request(this.path, "get", null)
-    }
-    query(databaseId, params){
-      this.parent.request(`${this.path}/${databaseId}`, "post", params)
-    }
-    retrieve(databaseId){
-      return this.parent.request(`${this.path}/${databaseId}`, "get", null)
-    }
+  constructor(parent) {
+    this.parent = parent;
+    this.path = "databases";
+  }
+  list() {
+    return this.parent.request(this.path, "get", null);
+  }
+  query(databaseId, params) {
+    this.parent.request(`${this.path}/${databaseId}`, "post", params);
+  }
+  retrieve(databaseId) {
+    return this.parent.request(`${this.path}/${databaseId}`, "get", null);
+  }
 }
 
 class BlocksChildrenEndpoint {
-      constructor(parent) {
-        this.parent = parent;
-        this.path = "blocks";
-        this.child = "children";
-    }
-  append(blockId, params){
-    return this.parent.request(`${this.path}/${blockId}/${this.child}`, "patch", params)
+  constructor(parent) {
+    this.parent = parent;
+    this.path = "blocks";
+    this.child = "children";
   }
-  list(blockId){
-    return this.parent.request(`${this.path}/${blockId}/${this.child}`, "get", null)
+  append(blockId, params) {
+    return this.parent.request(
+      `${this.path}/${blockId}/${this.child}`,
+      "patch",
+      params
+    );
+  }
+  list(blockId) {
+    return this.parent.request(
+      `${this.path}/${blockId}/${this.child}`,
+      "get",
+      null
+    );
   }
 }
 
 class BlocksEndpoint {
-    constructor(parent) {
-        this.request = parent.request;
-        this.children = new BlocksChildrenEndpoint(this);
-    }
+  constructor(parent) {
+    this.request = parent.request;
+    this.children = new BlocksChildrenEndpoint(this);
+  }
 }
